60f4cdc260af0830fa743127a494008ac8306e7a
Michał Kędzia <222463@edu.p.lodz.pl>
2020-05-20 23:41:06 +0200

    javadoc add

---
+++ Javadoc Client/type-search-index.js
@@ -0,0 +1,1 @@
+typeSearchIndex = [{"l":"All Classes","url":"allclasses-index.html"},{"p":"sample","l":"CommunicationMessage"},{"p":"sample","l":"ConnectionClass"},{"p":"sample","l":"Controller"},{"p":"sample","l":"LoginController"},{"p":"sample","l":"Main"},{"p":"sample","l":"CommunicationMessage.MessageType"}]
---
+++ Javadoc Server/type-search-index.js
@@ -0,0 +1,1 @@
+typeSearchIndex = [{"l":"All Classes","url":"allclasses-index.html"},{"p":"sample","l":"ClientCommunicationHandler"},{"p":"sample","l":"CommunicationMessage"},{"p":"sample","l":"Controller"},{"p":"sample","l":"Main"},{"p":"sample","l":"CommunicationMessage.MessageType"},{"p":"sample","l":"ServerClass"}]
